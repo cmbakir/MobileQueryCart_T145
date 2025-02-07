80b2308b427723b513e7fb21f65322d8122cd3ec
cembakir <cembakir@hotmail.com>
2025-02-07 13:21:21 +0300

    StepDef

--- src/test/java/stepdefinitions/Stepdefinition.java
+++ src/test/java/stepdefinitions/Stepdefinition.java
@@ -63,14 +63,16 @@ public class Stepdefinition extends OptionsMet {
         ReusableMethods.wait(1);
         VerifyElementText(userName);
     }
+
     /***US 11   **/
     @Given("As a user muss be {string} phone and {string} password Login")
     public void as_a_user_muss_be_phone_and_password_login(String phoneNumber, String password) {
-        card.Login(phoneNumber,password);
+        card.Login(phoneNumber, password);
     }
+
     @Given("User clicks the button with itemName {string} and {string} and {string} added WishList")
-    public void user_clicks_the_button_with_item_name_and_and_added_wish_list(String itemName, String reviews,String price) {
-       xPathElementClick(itemName,reviews,price);
+    public void user_clicks_the_button_with_item_name_and_and_added_wish_list(String itemName, String reviews, String price) {
+        xPathElementClick(itemName, reviews, price);
     }
 
 
@@ -78,38 +80,46 @@ public class Stepdefinition extends OptionsMet {
     public void driver_turns_off() {
         quitAppiumDriver();
     }
+
     @Given("Toaster is displayed")
     public void toast_is_displayed() {
         card.wishListToast();
 
     }
+
     @Given("User confirms that categories appear on the screen")
-    public void user_confirms_that_categories_appear_on_the_screen(){
-       // for (int i = 0; i <categoriesMen.size(); i++) {
+    public void user_confirms_that_categories_appear_on_the_screen() {
+        // for (int i = 0; i <categoriesMen.size(); i++) {
         //    assertTrue(categoriesMen.get(i).);
-      //  }
+        //  }
     }
 
     @Given("User clicks phone number textbox and {string} phone number")
     public void user_clicks_phone_number_textbox_and_phone_number(String phoneNumber) {
-     card. ForgetPasswordPhoneBox(phoneNumber);
+        card.ForgetPasswordPhoneBox(phoneNumber);
     }
+
     @Given("User clicks NewPasswordTextBox and confirmPasswordTextBox {string}")
     public void user_clicks_new_password_text_box_and_confirm_password_text_box(String newPassword) {
         card.NewPassword(newPassword);
     }
+
     @Given("User clicks tap coordinates {int}, {int}")
     public void user_clicks_tap_coordinates(Integer x, Integer y) {
         ReusableMethods.wait(1);
-        OptionsMet.touchDown(x,y);
+        OptionsMet.touchDown(x, y);
         ReusableMethods.wait(1);
     }
+
     @Given("User swipe to screen coordinates {int}, {int}, {int}, {int}")
     public void user_swipe_to_screen_coordinates(Integer x, Integer y, Integer endX, Integer endY) throws InvalidMidiDataException {
-        OptionsMet.swipe(x,y,endX,endY);
+        OptionsMet.swipe(x, y, endX, endY);
         ReusableMethods.wait(2);
+
+
     }
 
 
-}
+    }
+
 

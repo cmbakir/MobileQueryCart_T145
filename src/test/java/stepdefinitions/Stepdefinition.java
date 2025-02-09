a9e66c5e792d2d98c432feb14a39ccc3eddbb16f
cembakir <cembakir@hotmail.com>
2025-02-09 19:57:40 +0300

    cembakir/09.02.2025/Stepdefinition Updated

--- src/test/java/stepdefinitions/Stepdefinition.java
+++ src/test/java/stepdefinitions/Stepdefinition.java
@@ -1,15 +1,23 @@
 package stepdefinitions;
 
 import Page.QueryCardPage;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.ConfigReader;
 import utilities.OptionsMet;
 import utilities.ReusableMethods;
 
 
 import javax.sound.midi.InvalidMidiDataException;
+
+import java.time.Duration;
 
 import static org.junit.Assert.assertTrue;
 import static utilities.Driver.getAppiumDriver;
@@ -63,14 +71,16 @@ public class Stepdefinition extends OptionsMet {
         ReusableMethods.wait(1);
         VerifyElementText(userName);
     }
+
     /***US 11   **/
     @Given("As a user muss be {string} phone and {string} password Login")
     public void as_a_user_muss_be_phone_and_password_login(String phoneNumber, String password) {
-        card.Login(phoneNumber,password);
+        card.Login(phoneNumber, password);
     }
+
     @Given("User clicks the button with itemName {string} and {string} and {string} added WishList")
-    public void user_clicks_the_button_with_item_name_and_and_added_wish_list(String itemName, String reviews,String price) {
-       xPathElementClick(itemName,reviews,price);
+    public void user_clicks_the_button_with_item_name_and_and_added_wish_list(String itemName, String reviews, String price) {
+        xPathElementClick(itemName, reviews, price);
     }
 
 
@@ -78,38 +88,87 @@ public class Stepdefinition extends OptionsMet {
     public void driver_turns_off() {
         quitAppiumDriver();
     }
+
     @Given("Toaster is displayed")
     public void toast_is_displayed() {
         card.wishListToast();
 
     }
+
     @Given("User confirms that categories appear on the screen")
-    public void user_confirms_that_categories_appear_on_the_screen(){
-       // for (int i = 0; i <categoriesMen.size(); i++) {
+    public void user_confirms_that_categories_appear_on_the_screen() {
+        // for (int i = 0; i <categoriesMen.size(); i++) {
         //    assertTrue(categoriesMen.get(i).);
-      //  }
+        //  }
     }
 
     @Given("User clicks phone number textbox and {string} phone number")
     public void user_clicks_phone_number_textbox_and_phone_number(String phoneNumber) {
-     card. ForgetPasswordPhoneBox(phoneNumber);
+        card.ForgetPasswordPhoneBox(phoneNumber);
     }
+
     @Given("User clicks NewPasswordTextBox and confirmPasswordTextBox {string}")
     public void user_clicks_new_password_text_box_and_confirm_password_text_box(String newPassword) {
         card.NewPassword(newPassword);
     }
+
     @Given("User clicks tap coordinates {int}, {int}")
     public void user_clicks_tap_coordinates(Integer x, Integer y) {
         ReusableMethods.wait(1);
-        OptionsMet.touchDown(x,y);
+        OptionsMet.touchDown(x, y);
         ReusableMethods.wait(1);
     }
+
     @Given("User swipe to screen coordinates {int}, {int}, {int}, {int}")
     public void user_swipe_to_screen_coordinates(Integer x, Integer y, Integer endX, Integer endY) throws InvalidMidiDataException {
-        OptionsMet.swipe(x,y,endX,endY);
+        OptionsMet.swipe(x, y, endX, endY);
         ReusableMethods.wait(2);
     }
 
+    @Given("Verifies checkbox is selected")
+    public void verifies_checkbox_is_selected() {
+        card.getCheckBoxRememberMe().click();
+        ReusableMethods.wait(3);
+        assertTrue(card.getCheckBoxRememberMe().isEnabled());
 
+
+    }
+
+    @Given("Verifies to access to the forgot password page when it clicked")
+    public void verifies_to_access_to_the_forgot_password_page_when_it_clicked() {
+        clickButtonByDescription("Forgot Password");
+        getAppiumDriver().navigate().back();
+
+    }
+
+    @Given("Verifies to switch to the Register page from Log In Page")
+    public void verifies_to_switch_to_the_register_page_from_log_in_page() {
+        clickButtonByDescription("Sign Up");
+        clickButtonByDescription("*Use Email Instead");
+
+
+    }
+
+    @Given("User clicks the nameTextBox and sendKeys Name")
+    public void user_clicks_the_nameText_box_and_send_keys_name() {
+        nameTextBoxClickAndSendKeys("Cem2");
+    }
+
+    @Given("User clicks the emailTextBox and sendKeys Email")
+    public void user_clicks_the_email_text_box_and_send_keys_email() {
+        emailTextBoxClickAndSendKeys(ConfigReader.getProperty("cemNewUserEmail"));
+    }
+
+    @Given("User clicks the passwordTextBox and sendKeys Password")
+    public void user_clicks_the_password_text_box_and_send_keys_password() {
+        passwordTextBoxClickAndSendKeys(ConfigReader.getProperty("cemPassword"));
+    }
+
+    @Given("User clicks the button SignUp")
+    public void user_clicks_the_button_sign_up() {
+        card.getSignUpButton().click();
+        ReusableMethods.wait(10);
+        //assertTrue(card.getAlertSuccessMessageForSigningUp().getAttribute("content-desc").contains("Success"));
+    }
 }
 

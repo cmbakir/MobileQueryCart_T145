8ec31e1c86845d2d891d23c810923b9fd5462a51
pinarcay <pinarcay1984@gmail.com>
2025-02-19 17:24:10 +0300

    pinar/19.02/Runner.java Updated

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -6,13 +6,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty",
-                "json:target/cucumber.json",},
+                "json:target/cucumber.json", "json:target/cucumber-reports/CucumberTestReport.json",},//json rapor almak için "json:target/cucumber-reports/CucumberTestReport.json" eklendi.
         features = "src/test/resources/features",
-        glue = {"stepdefinitions", "Hooks"},
-        tags = " @categories" ,
+        glue = {"stepdefinitions", "Hooks"},//Appium server ı otomatik çalıştırmak için buraya hooks eklendi
+        monochrome = true,//Rapor için eklendi
+        tags = " @TP_01" ,
         dryRun = false
-
-
 
 )
 public class Runner {
